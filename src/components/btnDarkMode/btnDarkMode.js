9bed2845209fb207c208839884bf653ce3eff623
Danil Vorontsov <rojerr@MacBook-Air-Danil.local>
2023-08-24 13:35:31 +0600

    change dark-ligth theme

--- src/components/btnDarkMode/btnDarkMode.js
+++ src/components/btnDarkMode/btnDarkMode.js
@@ -1,4 +1,39 @@
-<button className="dark-mode-btn">
-  <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-  <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-</button>;
+import React, { useEffect, useRef, useState } from 'react';
+import './style.css';
+import sun from './sun.svg';
+import moon from './moon.svg';
+import { useLocalStorage } from '../../utils/useLocalStorage';
+import detectDarkMode from '../../utils/detectDarkMode';
+
+const BtnDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', detectDarkMode());
+
+  const btnRef = useRef(null);
+
+  useEffect(() => {
+    if (darkMode === 'dark') {
+      document.body.classList.add('dark');
+      btnRef.current.classList.add('dark-mode-btn--active');
+    } else {
+      document.body.classList.remove('dark');
+      btnRef.current.classList.remove('dark-mode-btn--active');
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((current) => {
+      return current === 'light' ? 'dark' : 'light';
+    });
+  };
+
+  return (
+    <>
+      <button ref={btnRef} onClick={toggleDarkMode} className="dark-mode-btn">
+        <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
+        <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+      </button>
+    </>
+  );
+};
+
+export default BtnDarkMode;
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,7 +3,6 @@ import { useParams } from 'react-router-dom';
 
 import BtnGitHub from '../components/btnGitHub/BtnGitHub';
 
-import img from './../img/projects/02-big.jpg';
 import projects from '../helpers/projectsList';
 
 const Project = () => {
